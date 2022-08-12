6dca2a7a07f98238d0c6f4da4898ff47c7d649f5
ibidapo-chad <ibidapo.chad@gmail.com>
2022-08-12 22:03:11 +0100

    Create Task_2 files

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Notifications.css'
+
+export default function notify () {
+   return (
+   <div class='Notifications'>
+        <p> Here is a list of notifications</p>
+    </div>
+   )
+}
