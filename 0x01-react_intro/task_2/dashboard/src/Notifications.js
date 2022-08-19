8ec4a8d3c9de6952a5e9777f050b7daa81f6011d
ibidapo-chad <ibidapo.chad@gmail.com>
2022-08-19 19:33:01 +0100

    Add more page structure

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -2,9 +2,17 @@ import React from 'react'
 import './Notifications.css'
 
 const Notifications= () => {
+
    return (
    <div className='Notifications'>
         <p> Here is a list of notifications</p>
+        <ul>
+         <li>New course available</li>
+         <li>New resume available</li>
+        </ul>
+        <button style={{color:"#ffffff"}} aria-label="Close" onClick={console.log("Close button has been clicked")}>
+         <img src='./favicon.ico' alt='close-button icon'/>
+        </button>
     </div>
    )
 }
