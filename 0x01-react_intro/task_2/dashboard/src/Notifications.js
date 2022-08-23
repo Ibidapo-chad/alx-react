e6b7d3a3b46a0de9553057cb9d9a18f079e992d7
ibidapo-chad <ibidapo.chad@gmail.com>
2022-08-23 12:47:34 +0100

    Update Notification fx with event handler

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -4,15 +4,16 @@ import { getLatestNotification } from './utils'
 
 const Notifications= () => {
 
+   let clickMessage = () => console.log ('Close-button has been clicked');
    return (
    <div className='Notifications'>
         <p> Here is a list of notifications</p>
         <ul>
-         <li>New course available</li>
-         <li>New resume available</li>
-         <getLatestNotifications />
+         <li data="default">New course available</li>
+         <li data="urgent">New resume available</li>
+         <li dangerouslySetInnerHTML={{__html: getLatestNotification()}} />
         </ul>
-        <button style={{color:'white'}} aria-label="Close" onClick={console.log("Close button has been clicked")}>
+        <button style={{color: '#ffffff'}} aria-label="Close" onClick={clickMessage}>
          <img src='./favicon.ico' alt='close-button icon'/>
         </button>
     </div>
