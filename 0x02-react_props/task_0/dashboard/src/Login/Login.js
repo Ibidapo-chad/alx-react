00003b90256e8f99e0b1452c293a564129deacc0
ibidapo-chad <ibidapo.chad@gmail.com>
2022-08-31 16:05:07 +0100

    Update Login fx

--- 0x02-react_props/task_0/dashboard/src/Login/Login.js
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.js
@@ -2,6 +2,11 @@ function Login() {
     return (
       <body className="App-body">
         <p>Login to access the full dashboard</p>
+        <label for="e-mail"> Email </label>
+        <input type="email" id="e-mail"/>
+        <label for="pass"> Password </label>
+        <input type="password" id="pass"/> 
+        <input type="button" value="OK"/>
       </body>
     )
 };
