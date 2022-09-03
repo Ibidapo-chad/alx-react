603e25ae49f5161b4771f01fe5e71a3c03aa06f4
ibidapo-chad <ibidapo.chad@gmail.com>
2022-09-03 22:28:25 +0100

    Populate with couse list

---
+++ 0x02-react_props/task_0/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CourseListRow from './CourseListRow';
+
+function CourseList(props) {
+    return (
+        <thead>
+            <CourseListRow textFirstCell="Available courses" isHeader={true} />
+            <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+        <tbody>
+            <CourseListRow textFirstCell="ES6" textSecondCell="60" isHeader={false}/>
+            <CourseListRow textFirstCell="webpack" textSecondCell="20" isHeader={false}/>
+            <CourseListRow textFirstCell="React" textSecondCell="40" isHeader={false}/>
+        </tbody>
+        </thead>
+    )
+};
+
+export default CourseList
