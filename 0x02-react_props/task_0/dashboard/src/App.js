5d155fdf6b53498f604948108939adff83705e5d
ibidapo-chad <ibidapo.chad@gmail.com>
2022-09-03 22:35:46 +0100

    Update App code to conditionally render body element

--- 0x02-react_props/task_0/dashboard/src/App.js
+++ 0x02-react_props/task_0/dashboard/src/App.js
@@ -4,16 +4,26 @@ import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Login from './Login/Login';
 import Notifications from './Notifications';
+import CourseList from './CourseList/CourseList';
+import PropTypes from 'prop-types';
 
-function App() {
+function App({isLoggedIn}) {
   return (
     <React.Fragment>
       <Notifications />
       <Header />
-      <Login />
+      {isLoggedIn} ? <Login /> : <CourseList />
       <Footer />
     </React.Fragment>
   )
 };
+
+App.propTypes = {
+  isLoggedIn : bool
+}
+
+App.defaultProps = {
+  isLoggedIn : false
+}
 
 export default App;
