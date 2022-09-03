f15758831683553992e1734dbedfc66216c4ae3b
ibidapo-chad <ibidapo.chad@gmail.com>
2022-09-03 18:57:51 +0100

    Fix conditional rendering code

--- 0x02-react_props/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x02-react_props/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,20 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function CourseListRow({ isHeader = false, textSecondCell = null, textFirstCell }) {
+function CourseListRow({ isHeader, textSecondCell, textFirstCell }) {
     return (
         <tr>
-            {if (isHeader) {
-                if (textSecondCell=null) {
-                    return <th colSpan="2">props.textFirstCell</th>
-                }
-            }}
+            { (() => {
+                        if ({isHeader}) {
+                            if ({textSecondCell}===null)
+                                return (<th colSpan="2">{textFirstCell}</th>)
+                            if ({textSecondCell} !== null) {
+                                return (
+                                    <>
+                                        <th> {textFirstCell} </th>
+                                        <th> {textSecondCell} </th>
+                                    </>
+                                )
+                            }
+                        } else {
+                            return (
+                                <>
+                                    <td> {textFirstCell} </td>
+                                    <td> {textSecondCell} </td> 
+                                </>
+                            )
+                        }
+            })()
+            }
         </tr>
-
-        CourseListRow.PropTypes = {
-            isHeader : PropTypes.bool,
-            textFirstCell : PropTypes.string.isRequired,
-            textSecondCell: PropTypes.string
-        }
     )
-}
+};
+
+CourseListRow.PropTypes = {
+    isHeader : PropTypes.bool,
+    textFirstCell : PropTypes.string.isRequired,
+    textSecondCell: PropTypes.string
+};
+
+CourseListRow.defaultProps = {
+    isHeader : false,
+    textSecondCell : null
+};
+
+export default CourseListRow
+
+//Alternative code
+/**function CourseListRow({ isHeader, textSecondCell, textFirstCell }) {
+    return (
+        <tr>
+            {isHeader} ? 
+                ({textSecondCell === null} ? 
+                    <th colSpan={2}>{textFirstCell}</th>
+                {textSecondCell !== null} ? 
+                    <th> {textFirstCell} </th>
+                    <th> {textSecondCell} </th>)
+                :
+               ( <>
+                    <td> {textFirstCell} </td>
+                    <td> {textSecondCell} </td> 
+                </> )
+        </tr>
+    )
+}*/
