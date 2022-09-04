d548fe13f1511334b17adc80f2ccfb70eb0e4a4e
ibidapo-chad <ibidapo.chad@gmail.com>
2022-09-04 23:58:13 +0100

    Fix conditional statements

--- 0x02-react_props/task_0/dashboard/src/Notifications.js
+++ 0x02-react_props/task_0/dashboard/src/Notifications.js
@@ -2,23 +2,35 @@ import React from 'react'
 import './Notifications.css'
 import { getLatestNotification } from './utils'
 import NotificationItem from './NotificationItem'
+import PropTypes from 'prop-types';
 
-const Notifications= () => {
+const Notifications= ({displayDrawer}) => {
 
    let clickMessage = () => console.log ('Close-button has been clicked');
    return (
-   <div className='Notifications'>
-        <p> Here is a list of notifications</p>
-        <ul>
-         <NotificationItem type="default" value="New course available" html=""/>
-         <NotificationItem type="urgent" value="New resume available" html=""/>
-         <NotificationItem type="" value="" html={getLatestNotification()}/>
-        </ul>
-        <button style={{color: '#ffffff'}} aria-label="Close" onClick={clickMessage}>
-         <img src='./favicon.ico' alt='close-button icon'/>
-        </button>
-    </div>
+      <>
+         <div className='menuItem'>Your notifications</div>
+         { displayDrawer===false ? null : 
+            (<div className='Notifications'>
+               <p> Here is a list of notifications</p>
+               <ul>
+                  <NotificationItem type="default" value="New course available" html=""/>
+                  <NotificationItem type="urgent" value="New resume available" html=""/>
+                  <NotificationItem type="" value="" html={getLatestNotification()}/>
+               </ul>
+               <button style={{color: '#ffffff'}} aria-label="Close" onClick={clickMessage}>
+               <img src='./favicon.ico' alt='close-button icon'/>
+               </button>
+            </div>)}
+      </>
    )
 };
 
-export default Notifications;
+Notifications.propTypes = {
+   displayDrawer : PropTypes.bool
+}
+Notifications.defaultProps = {
+   displayDrawer : false
+}
+
+export default Notifications
