854e10da679e27ca53568dc1e84b248a3ce559f1
ibidapo-chad <ibidapo.chad@gmail.com>
2022-09-05 23:44:51 +0100

    Update function courseList

--- 0x02-react_props/task_0/dashboard/src/CourseList/CourseList.js
+++ 0x02-react_props/task_0/dashboard/src/CourseList/CourseList.js
@@ -3,15 +3,17 @@ import CourseListRow from './CourseListRow';
 
 function CourseList(props) {
     return (
-        <thead>
-            <CourseListRow textFirstCell="Available courses" isHeader={true} />
-            <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
-        <tbody>
-            <CourseListRow textFirstCell="ES6" textSecondCell="60" isHeader={false}/>
-            <CourseListRow textFirstCell="webpack" textSecondCell="20" isHeader={false}/>
-            <CourseListRow textFirstCell="React" textSecondCell="40" isHeader={false}/>
-        </tbody>
-        </thead>
+        <>
+            <thead>
+                <CourseListRow textFirstCell="Available courses" isHeader={true} />
+                <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+            </thead>
+            <tbody>
+                <CourseListRow textFirstCell="ES6" textSecondCell="60" isHeader={false}/>
+                <CourseListRow textFirstCell="webpack" textSecondCell="20" isHeader={false}/>
+                <CourseListRow textFirstCell="React" textSecondCell="40" isHeader={false}/>
+            </tbody>
+        </>
     )
 };
 
