4d484b56796ef6b9ae2d0889e853b0cbe56a8000
ibidapo-chad <ibidapo.chad@gmail.com>
2022-09-08 08:07:59 +0100

    Optimize component function using arrays

--- 0x02-react_props/task_3/dashboard/src/CourseList/CourseList.js
+++ 0x02-react_props/task_3/dashboard/src/CourseList/CourseList.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Courseshape from './CourseShape';
 import CourseListRow from './CourseListRow';
+import './CourseShape';
 
-function CourseList(props) {
+function CourseList({listCourses}) {
     return (
         <>
             <thead>
@@ -9,12 +12,33 @@ function CourseList(props) {
                 <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
             </thead>
             <tbody>
-                <CourseListRow textFirstCell="ES6" textSecondCell="60" isHeader={false}/>
-                <CourseListRow textFirstCell="webpack" textSecondCell="20" isHeader={false}/>
-                <CourseListRow textFirstCell="React" textSecondCell="40" isHeader={false}/>
+              {
+                listCourses.length > 0 ? (
+                    listCourses.map((course) => {
+                        return(
+                            <CourseListRow 
+                                key={course.id}
+                                textFirstCell={course.name}
+                                textSecondCell={course.credit}
+                            />
+                        )
+                    })
+                ) :
+                (
+                    <CourseListRow textFirstCell='No course available yet' />
+                )
+              }  
             </tbody>
         </>
     )
+};
+
+CourseList.propTypes = {
+    listCourses: PropTypes.arrayOf(Courseshape)
+};
+
+CourseList.defaultProps = {
+    listCourses: []
 };
 
 export default CourseList
