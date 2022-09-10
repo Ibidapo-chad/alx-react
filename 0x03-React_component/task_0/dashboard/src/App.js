a03b7a1dd0e055d21e82d12f5d507ef289086818
ibidapo-chad <ibidapo.chad@gmail.com>
2022-09-10 10:41:45 +0100

    Convert function-based component to class-based component

--- 0x03-React_component/task_0/dashboard/src/App.js
+++ 0x03-React_component/task_0/dashboard/src/App.js
@@ -8,7 +8,7 @@ import CourseList from './CourseList/CourseList';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from './utils';
 
-function App({isLoggedIn}) {
+/**function App({isLoggedIn}) {
   const datar = [{id: 1, name: 'ES6', credit: 60}, 
     {id: 2, name: 'Webpack', credit: 20}, 
     {id: 3, name: 'React', credit: 40}];
@@ -25,7 +25,32 @@ function App({isLoggedIn}) {
       <Footer />
     </React.Fragment>
   )
-};
+};*/
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {isLoggedIn: false}; //check how proptypes can be used in class based components
+    this.notificationData = [{id: 1, type: 'default', value: 'New course available', html: ''},
+      {id: 2, type: 'urgent', value: 'New resume available', html: ''},
+      {id: 3, type: 'default', value: '', html: getLatestNotification()}];
+    this.datar = [{id: 1, name: 'ES6', credit: 60}, 
+      {id: 2, name: 'Webpack', credit: 20}, 
+      {id: 3, name: 'React', credit: 40}];
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Notifications listnotifications={this.notificationData}/>
+        <Header />
+        { this.state.isLoggedIn ? <Login /> : <CourseList listCourses={this.datar}/>}
+        <Footer />
+      </React.Fragment>
+    )
+  }
+}
 
 App.propTypes = {
   isLoggedIn : PropTypes.bool
