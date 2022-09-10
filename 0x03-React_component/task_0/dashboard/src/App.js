9367b8a4e3bb2db969a1626788bfceb3ffc64e01
ibidapo-chad <ibidapo.chad@gmail.com>
2022-09-10 10:53:54 +0100

    Organize order of code

--- 0x03-React_component/task_0/dashboard/src/App.js
+++ 0x03-React_component/task_0/dashboard/src/App.js
@@ -8,27 +8,6 @@ import CourseList from './CourseList/CourseList';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from './utils';
 
-/**function App({isLoggedIn}) {
-  const datar = [{id: 1, name: 'ES6', credit: 60}, 
-    {id: 2, name: 'Webpack', credit: 20}, 
-    {id: 3, name: 'React', credit: 40}];
-
-  const notificationData = [{id: 1, type: 'default', value: 'New course available', html: ''},
-    {id: 2, type: 'urgent', value: 'New resume available', html: ''},
-    {id: 3, type: 'default', value: '', html: getLatestNotification()}];
-   
-  return (
-    <React.Fragment>
-      <Notifications listnotifications={notificationData}/>
-      <Header />
-      { isLoggedIn ? <Login /> : <CourseList listCourses={datar}/>}
-      <Footer />
-    </React.Fragment>
-  )
-};*/
-
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -51,6 +30,27 @@ class App extends React.Component {
     )
   }
 }
+
+
+/**function App({isLoggedIn}) {
+  const datar = [{id: 1, name: 'ES6', credit: 60}, 
+    {id: 2, name: 'Webpack', credit: 20}, 
+    {id: 3, name: 'React', credit: 40}];
+
+  const notificationData = [{id: 1, type: 'default', value: 'New course available', html: ''},
+    {id: 2, type: 'urgent', value: 'New resume available', html: ''},
+    {id: 3, type: 'default', value: '', html: getLatestNotification()}];
+   
+  return (
+    <React.Fragment>
+      <Notifications listnotifications={notificationData}/>
+      <Header />
+      { isLoggedIn ? <Login /> : <CourseList listCourses={datar}/>}
+      <Footer />
+    </React.Fragment>
+  )
+};*/
+
 
 App.propTypes = {
   isLoggedIn : PropTypes.bool
