bfec24e326a3d36adb37300f7f3fc975b71d8739
ibidapo-chad <ibidapo.chad@gmail.com>
2022-11-17 00:00:59 +0100

    Change Html props data type

--- 0x05-react_state/task_2/dashboard/src/NotificationItemShape.js
+++ 0x05-react_state/task_2/dashboard/src/NotificationItemShape.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const NotificationItemShape = PropTypes.shape({
     id: PropTypes.number.isRequired,
-    html: PropTypes.objectOf( PropTypes.string ),
+    html: PropTypes.string,
     type: PropTypes.string.isRequired,
     value: PropTypes.string
 });
