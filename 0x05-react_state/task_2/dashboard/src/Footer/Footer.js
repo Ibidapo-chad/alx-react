cd0cf014c844e882e8388f24a097051895f8f7a8
ibidapo-chad <ibidapo.chad@gmail.com>
2022-11-19 23:52:04 +0100

    Pass context into Footer fx component

--- 0x05-react_state/task_2/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_2/dashboard/src/Footer/Footer.js
@@ -1,12 +1,24 @@
 import { getFooterCopy, getFullYear } from '../utils'
 import React from 'react';
+import { AppContext } from '../AppContext';
 
 function Footer() {
     return (
-      <footer className="App-footer">
-        <p>Copyright {getFooterCopy()} - {getFullYear()}</p>
-      </footer>
-    )
+      <AppContext.Consumer>
+      {
+        (context) => {
+          return (
+            <footer className="Footer">
+              <p>
+                <i>Copyright {getFullYear()} - {getFooterCopy()}</i>
+              </p>
+              {context.user.isLoggedIn && <a href='#'>Contact us</a>}
+            </footer>
+          );
+        }
+      }
+    </AppContext.Consumer>
+	)
 };
 
 export default Footer;
