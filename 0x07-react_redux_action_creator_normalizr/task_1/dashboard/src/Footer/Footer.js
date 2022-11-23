99702d09b5bc346e86c956233e96c9d0c57bffd1
ibidapo-chad <ibidapo.chad@gmail.com>
2022-11-23 02:45:52 +0100

    update project files for new task

---
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/Footer/Footer.js
@@ -0,0 +1,25 @@
+import { getFooterCopy, getFullYear } from '../utils'
+import React from 'react';
+import { AppContext } from '../AppContext';
+
+function Footer() {
+    return (
+      <AppContext.Consumer>
+      {
+        (context) => {
+          return (
+            <footer className="Footer">
+              <p>
+                <i>Copyright {getFullYear()} - {getFooterCopy()}.</i>
+                &nbsp;
+                {context.user.isLoggedIn && <a href='#'>Contact us</a>}
+              </p>
+            </footer>
+          );
+        }
+      }
+    </AppContext.Consumer>
+	)
+};
+
+export default Footer;
