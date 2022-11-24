b4487082cab96749ad5e94f2f5fc57a957597af9
ibidapo-chad <ibidapo.chad@gmail.com>
2022-11-24 01:04:28 +0100

    feat: Update fx to use normalized data

--- 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -1,4 +1,3 @@
-import * as notificationdata from '../../notifications.json'
 import { normalize, schema } from 'normalizr';
 
 const user = new schema.Entity('users');
@@ -11,7 +10,17 @@ const notification = new schema.Entity('notification', {
 const normalized = normalize(notificationData, [notification]);
 
 export default function getAllNotificationsByUser(userId) {
-    return notificationdata
-        .filter(notification => {notification.author.id === userId})
-        .map(notification => notification.context)
+	const output = [];
+	const notifications = normalized.entities.notification;
+	const messages = normalized.entities.messages;
+
+	for (const id in notifications) {
+		if (notifications[id].author === userId) {
+			output.push(messages[notifications[id].context]);
+		}
+	}
+
+	return output;
 }
+
+export { normalized };
