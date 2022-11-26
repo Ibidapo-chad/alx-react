39fcd5dd7f4375be3307f71f625e64b240a116a9
ibidapo-chad <ibidapo.chad@gmail.com>
2022-11-26 17:02:33 +0100

    feat: Update UI reducer to use Immutable data structure

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,3 +1,5 @@
+import {Map} from 'immutable';
+
 import { DISPLAY_NOTIFICATION_DRAWER, 
             HIDE_NOTIFICATION_DRAWER,
             LOGIN_SUCCESS,
@@ -11,18 +13,23 @@ const initialState= {
     user: {}
 }
 
-export default function uiReducer(state = initialState, action) {
+export default function uiReducer(state = Map (initialState), action) {
 	switch (action.type) {
 		case DISPLAY_NOTIFICATION_DRAWER:
-			return { ...state, isNotificationDrawerVisible: true };
+      		return state.set("isNotificationDrawerVisible", true);
+
 		case HIDE_NOTIFICATION_DRAWER:
-			return { ...state, isNotificationDrawerVisible: false };
+			return state.set("isNotificationDrawerVisible", false);
+
 		case LOGIN_SUCCESS:
-			return { ...state, isUserLoggedIn: true };
+			return state.set("isUserLoggedIn", true);
+
 		case LOGIN_FAILURE:
-			return { ...state, isUserLoggedIn: false };
+			return state.set("isUserLoggedIn", false);
+
 		case LOGOUT:
-			return { ...state, isUserLoggedIn: false };
+			return state.set("isUserLoggedIn", false);
+
 		default:
 			return state;
     }
