c627db5b9a59e7d7bca8395183cb68075167921d
ibidapo-chad <ibidapo.chad@gmail.com>
2022-11-28 11:06:42 +0100

    Updates

---
+++ precache-manifest.37e75119d6f02cff2443d4643c241c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "563fc2b2e194a3b4ed05",
+    "url": "/alx-react/static/css/main.4f544c0f.chunk.css"
+  },
+  {
+    "revision": "563fc2b2e194a3b4ed05",
+    "url": "/alx-react/static/js/main.3718b8e1.chunk.js"
+  },
+  {
+    "revision": "fad8ea637e6b8317b1f3",
+    "url": "/alx-react/static/js/runtime~main.f509b2d2.js"
+  },
+  {
+    "revision": "b80b85653126b6ce86b2",
+    "url": "/alx-react/static/js/2.37ed5249.chunk.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/alx-react/static/media/Holberton-logo.23d20eb3.jpg"
+  },
+  {
+    "revision": "1eb5431ff42f2a9596ef0bbc17bbe7c1",
+    "url": "/alx-react/index.html"
+  }
+];
