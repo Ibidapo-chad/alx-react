ce55b123cfd7f65de426f0c0f4a202e29676a2e1
ibidapo-chad <ibidapo.chad@gmail.com>
2023-01-13 17:39:10 +0100

    Update config files with webpack server install

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,37 +1,66 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+	plugins: [
+		new HTMLWebpackPlugin({
+			filename: './index.html',
+		}),
+		new CleanWebpackPlugin(),
+	],
+	devtool: 'inline-source-map',
+	mode: 'development',
 	entry: {
-		main: path.resolve(__dirname, './js/dashboard_main.js'),
+		header: {
+			import: './modules/header/header.js',
+			dependOn: 'shared',
+		},
+		body: {
+			import: './modules/body/body.js',
+			dependOn: 'shared',
+		},
+		footer: {
+			import: './modules/footer/footer.js',
+			dependOn: 'shared',
+		},
+		shared: 'jquery',
 	},
 	output: {
 		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
+		filename: '[name].bundle.js',
 	},
-    mode: 'production',
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
+	devServer: {
+		static: path.join(__dirname, './public'),
+		open: true,
+		port: 8564,
+	},
 	performance: {
 		maxAssetSize: 1000000,
 	},
 	module: {
 		rules: [
-		  {
-			test: /\.css$/i,
-			use: ['style-loader', 'css-loader'],
-		  },
-		  {
-			test: /\.(png|svg|jpg|jpeg|gif)$/i,
-			type: 'asset/resource',
-			use: [
-				'file-loader',
-				{
-				  loader: 'image-webpack-loader',
-				  options: {
-					bypassOnDebug: true,
-					disable: true,
-				  },
-				},
-			  ],
-		  },
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				use: [
+					{
+						loader: ['file-loader', 'image-webpack-loader'],
+						options: {
+							bypassOnDebug: true,
+							disable: true,
+						},
+					},
+				],
+			},
 		],
-	  },
+	},
 };
