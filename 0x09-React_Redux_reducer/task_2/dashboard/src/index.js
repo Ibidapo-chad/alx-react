3925753c5b57918f50c2b301dec0bb8cee5c0042
ibidapo-chad <ibidapo.chad@gmail.com>
2023-01-20 19:36:42 +0100

    fix: Update App import

--- 0x09-React_Redux_reducer/task_2/dashboard/src/index.js
+++ 0x09-React_Redux_reducer/task_2/dashboard/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
-import App from "./App";
+import App from "./App/App";
 import uiReducer, { initialState } from "./reducers/uiReducer";
 import { Map } from "immutable";
 
