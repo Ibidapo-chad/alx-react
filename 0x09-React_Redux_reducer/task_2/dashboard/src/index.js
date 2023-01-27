b43adc5b59aee4c1334139010d0593a11b7fd5f9
ibidapo-chad <ibidapo.chad@gmail.com>
2023-01-27 18:21:00 +0100

    feat: Add Redux devtools config

--- 0x09-React_Redux_reducer/task_2/dashboard/src/index.js
+++ 0x09-React_Redux_reducer/task_2/dashboard/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import App from "./App/App";
 import uiReducer, { initialState } from "./reducers/uiReducer";
 import { Map } from "immutable";
 
-const store = createStore(uiReducer, Map(initialState), applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(uiReducer, Map(initialState), composeEnhancers(applyMiddleware(thunk)) );
 
 ReactDOM.render(
   <React.StrictMode>
