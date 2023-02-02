f70a1d9bdaed779e33ec6acd8195b87a3154a633
ibidapo-chad <ibidapo.chad@gmail.com>
2023-02-02 23:41:23 +0100

    update all project files in task 3

---
+++ 0x09-React_Redux_reducer/task_3/dashboard/src/schema/notifications.js
@@ -0,0 +1,30 @@
+import { normalize, schema } from 'normalizr';
+import * as notificationData from '../../public/notifications.json'
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notification', {
+	author: user,
+	context: message,
+});
+
+const normalized = normalize(notificationData, [notification]);
+
+export default function getAllNotificationsByUser(userId) {
+	const output = [];
+	const notifications = normalized.entities.notification;
+	const messages = normalized.entities.messages;
+
+	for (const id in notifications) {
+		if (notifications[id].author === userId) {
+			output.push(messages[notifications[id].context]);
+		}
+	} //confirm the form of notifications
+	return output;
+}
+
+export { normalized };
+
+export function notificationsNormalizer(data) {
+	return normalize(data, [notification]);
+}
