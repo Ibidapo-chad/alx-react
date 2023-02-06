591b4c5c5e2f1b26884d549e42021b941397fa95
ibidapo-chad <ibidapo.chad@gmail.com>
2023-02-06 21:26:54 +0100

    refactor arguments in createstore fx

--- 0x09-React_Redux_reducer/task_4/dashboard/src/index.js
+++ 0x09-React_Redux_reducer/task_4/dashboard/src/index.js
@@ -1,27 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import App from "./App/App";
-import uiReducer, { initialState } from "./reducers/uiReducer";
-import { Map } from "immutable";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import App from './App/App';
+import rootReducer, { initialRootState } from './reducers/rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(uiReducer, Map(initialState), composeEnhancers(applyMiddleware(thunk)) );
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const store = createStore(
+	rootReducer,
+	initialRootState,
+	composeEnhancers(applyMiddleware(thunk))
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
