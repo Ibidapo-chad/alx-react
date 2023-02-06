4b53be99e0cc52f31f98ca5f76782c4c6fe5e1a9
ibidapo-chad <ibidapo.chad@gmail.com>
2023-02-06 21:49:43 +0100

    update project files for task 5 commits

---
+++ 0x09-React_Redux_reducer/task_5/dashboard/src/reducers/notificationReducer.js
@@ -0,0 +1,49 @@
+import { Map } from 'immutable';
+import { notificationsNormalizer } from '../schema/notifications';
+import {
+	FETCH_NOTIFICATIONS_SUCCESS,
+	MARK_AS_READ,
+	SET_TYPE_FILTER,
+} from '../actions/notificationActionTypes';
+
+
+export const initialNotificationState = {
+	notifications: [],
+	filter: 'DEFAULT',
+};
+
+const notificationReducer= (state = Map(initialNotificationState), action) => {
+	switch (action.type) {
+		case FETCH_NOTIFICATIONS_SUCCESS:
+			const notificationData = action.data.map((item) => ({
+				id: item.id,
+				type: item.type,
+				value: item.value,
+				isRead: false,
+			}));
+			return state.merge({
+				filter: NotificationTypeFilters.DEFAULT,
+				notifications: notificationsNormalizer(notificationData),
+			});
+		
+		case MARK_AS_READ: {
+			return state.setIn(
+				[
+					'notifications',
+					'entities',
+					'notifications',
+					action.index.toString(),
+					'isRead',
+				],
+				true
+			);
+		}
+		case SET_TYPE_FILTER: {
+			return state.set('filter', action.filter);
+		}
+		default:
+			return state;
+	}
+};
+
+export default notificationReducer;
